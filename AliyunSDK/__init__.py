11836bdb9deb6de9eeb8de315bfbd0ca767d3d92
leonanu <nanu@inanu.net>
2016-06-06 23:39:35 +0800

    update README

--- AliyunSDK/__init__.py
+++ AliyunSDK/__init__.py
@@ -6,14 +6,13 @@ Aliyun SDK library
 AliyunSDK is a Aliyun ECS, SLB and RDS API SDK library, written in Python.
 
 usage:
-   >>> from AliyunSDK import rds
-   >>> r = rds.AliyunRDS(access_id, access_secret)
-   >>> r.show_regions()
+   >>> import AliyunSDK
+   >>> rds = AliyunSDK.RDS(access_id, access_secret)
+   >>> rds.show_region_list()
 '''
 
 from rds import RDS
 
 
 __title__ = 'AliyunSDK'
-__version__ = '1.0.0'
 __author__ = 'Nanu'
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # AliyunSDK
-Aliyun API SDK written in Python
 
-On developing ...
+AliyunSDK is a Aliyun ECS, SLB and RDS API SDK library, written in Python.       
+                                                                                 
+** usage:
+```
+    >>> import AliyunSDK
+    >>> rds = AliyunSDK.RDS(access_id, access_secret)
+    >>> rds.show_region_list()
+```
