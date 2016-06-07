22c407fc530c601137f23a3d5e1f4d10ff687c46
leonanu <nanu@inanu.net>
2016-06-07 11:04:49 +0800

    package setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='AliyunSDK',
+    version='1.0',
+    packages=['AliyunSDK'],
+    url='https://github.com/leonanu/AliyunSDK',
+    license='MIT',
+    author='Nanu',
+    author_email='nanu@inanu.net',
+    description='AliyunSDK is a Aliyun ECS, SLB and RDS API SDK library',
+    download_url='https://github.com/leonanu/AliyunSDK/archive/master.zip',
+    classifiers=[
+            'Development Status :: 1 - Planning',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            ],
+)
